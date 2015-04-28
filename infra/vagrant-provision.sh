a76db5d57657755a3a98b75e2b22a29cafdf1260
Pete Hodgson <git@thepete.net>
2015-04-27 20:25:16 -0700

    basic vagrant with neo4j

---
+++ infra/vagrant-provision.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function install_neo4j() {
+  export DEBIAN_FRONTEND=noninteractive
+  wget -O - http://debian.neo4j.org/neotechnology.gpg.key | apt-key add -
+  echo 'deb http://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.list
+  apt-get update
+  apt-get -y install neo4j
+}
+which neo4j-shell > /dev/null || install_neo4j
