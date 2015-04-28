5c77da7a9a36c9c05c86661b8870d800cb602f02
Pete Hodgson <git@thepete.net>
2015-04-27 21:34:04 -0700

    basic splunk CSV parsing

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem 'neography', '~> 1.7.2'
+
+gem 'pry-nav'
--- infra/vagrant-provision.sh
+++ infra/vagrant-provision.sh
@@ -8,3 +8,9 @@ function install_neo4j() {
   apt-get -y install neo4j
 }
 which neo4j-shell > /dev/null || install_neo4j
+
+#function setup_ruby() {
+  #apt-get install ruby2.0
+  #gem2.0 install bundler
+#}
+#setup_ruby
