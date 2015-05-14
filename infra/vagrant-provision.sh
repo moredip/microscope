eed9d99dfcc8bd4f727e2f5582e238c175e97763
Pete Hodgson <git@thepete.net>
2015-05-14 07:04:05 -0700

    vagrant tweaks

--- infra/vagrant-provision.sh
+++ infra/vagrant-provision.sh
@@ -6,11 +6,16 @@ function install_neo4j() {
   echo 'deb http://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.list
   apt-get update
   apt-get -y install neo4j
+
+  echo 'org.neo4j.server.webserver.address=0.0.0.0' >> /etc/neo4j/neo4j-server.properties
+  echo 'dbms.security.auth_enabled=false' >> /etc/neo4j/neo4j-server.properties
 }
 which neo4j-shell > /dev/null || install_neo4j
 
-#function setup_ruby() {
-  #apt-get install ruby2.0
-  #gem2.0 install bundler
-#}
-#setup_ruby
+setup_ruby() {
+  apt-get install -y build-essential ruby-dev
+  cd /vagrant
+  gem install bundler
+  bundle install --system
+}
+setup_ruby
