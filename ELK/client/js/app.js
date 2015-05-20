bd1492e5eedeb2a583c37b3d070cfba7f09d6c50
Pete Hodgson <git@thepete.net>
2015-05-19 19:41:32 -0700

    move to react

--- ELK/client/js/app.js
+++ ELK/client/js/app.js
@@ -1,4 +1,3 @@
-
 function traversedSpans(rootSpan){
   var spans = [];
   ES.visitSpanTreeDepthFirstPreOrder(rootSpan, function(span){
@@ -7,55 +6,79 @@ function traversedSpans(rootSpan){
   return spans;
 }
 
+var WaterfallLine = React.createFactory(React.createClass({
+  render: function() {
+    return React.DOM;
+  }
+}));
+
+var WaterfallChart = React.createFactory(React.createClass({
+  getDefaultProps: function(){
+    var margin = {top: 1, right: 1, bottom: 6, left: 1};
+    return {
+      margin: margin,
+      width: 960 - margin.left - margin.right,
+      height: 800 - margin.top - margin.bottom,
+      rowHeight: 30,
+      rowVertPadding: 5
+    };
+  },
+
+  renderWaterfallLines: function(){
+    var props = this.props;
+
+    var rootSpan = ES.findRootSpan(props.spans);
+    var sequencedSpans = traversedSpans(rootSpan);
+
+    var color = d3.scale.category20();
+    var x = d3.scale.linear()
+      .domain([0,rootSpan.elapsedMillis])
+      .range([0,props.width]);
+
+    var rowHeightWithPadding = props.rowHeight + props.rowVertPadding;
+
+    var baseTimeOffset = rootSpan.startTime;
+
+    return _.map(sequencedSpans,function(span,ix){
+      return React.DOM.g( {
+        transform: "translate(" + x(5+span.startTime-baseTimeOffset) + "," + (ix*rowHeightWithPadding) + ")"
+      },
+        React.DOM.rect({
+          height: props.rowHeight,
+          width: x(span.elapsedMillis),
+          fill: span.color = color(span.name.replace(/ .*/, "")),
+          style: {stroke: d3.rgb(span.color).darker(2)}
+        }),
+        React.DOM.text({
+          x: "1em",
+          dy: props.rowHeight/2,
+          "text-anchor": "left"
+        }, span.name )
+      );
+    });
+  },
+
+  render: function() {
+
+
+    return React.DOM.svg( {
+      width: this.props.width + this.props.margin.left + this.props.margin.right,
+      height: this.props.height + this.props.margin.top + this.props.margin.bottom
+    },
+      React.DOM.g({
+        transform: "translate(" + this.props.margin.left + "," + this.props.margin.top + ")"
+      },
+        this.renderWaterfallLines()
+      )
+    );
+  }
+}));
+
 function renderTraceSpanTree(spans){
-
-  var rootSpan = ES.findRootSpan(spans);
-  var sequencedSpans = traversedSpans(rootSpan);
-
-  console.log( 'root', rootSpan );
-  console.log( 'sequenced spans', sequencedSpans );
-
-  var margin = {top: 1, right: 1, bottom: 6, left: 1},
-    width = 960 - margin.left - margin.right,
-    height = 800 - margin.top - margin.bottom,
-    rowHeight = 40, rowVertPadding = 5,
-    rowHeightWithPadding = rowHeight + rowVertPadding;
-
-  var color = d3.scale.category20();
-
-  var x = d3.scale.linear()
-    .domain([0,rootSpan.elapsedMillis])
-    .range([0,width]);
-
-  var baseTimeOffset = rootSpan.startTime;
-
-  var svg = d3.select("#chart").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  var span = svg.append("g").selectAll(".span")
-    .data(sequencedSpans)
-  .enter().append("g")
-    .attr("class", "span")
-    .attr("transform", function(d,ix) { return "translate(" + x(5+d.startTime-baseTimeOffset) + "," + (ix*rowHeightWithPadding) + ")"; })
-
-  span.append("rect")
-    .attr("height", rowHeight)
-    .attr("width", function(d) { return x(d.elapsedMillis-10); })
-    .style("fill", function(d) { return d.color = color(d.name.replace(/ .*/, "")); })
-    .style("stroke", function(d) { return d3.rgb(d.color).darker(2); })
-  .append("title")
-    .text(function(d) { return d.name; });
-
-  span.append("text")
-      .attr("x", "1em")
-      .attr("dy", rowHeight/2)
-      .attr("text-anchor", "left")
-      .text(function(d) { return d.name; })
-      //.style("opacity", function(d) { d.w = this.getComputedTextLength(); return d.dx > d.w ? 1 : 0; });
-  
+  React.render(
+      WaterfallChart({spans:spans}),
+      document.getElementById('chart')
+  );
 }
 
 var traceId = window.location.search.substr(1);
